37c6859b3e8cdbbf99500b64cbc8ae628b714420
RyanClarker <clarke.ryan24@gmail.com>
2014-04-29 10:12:33 -0400

    new version

---
+++ Asteroid Belt Assault/Planets.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Asteroid_Belt_Assault
+{
+    class Planets
+    {
+        private List<Sprite> planets = new List<Sprite>();
+        private int screenWidth = 800;
+        private int screenHeight = 600;
+        private Random rand = new Random();
+        private Color[] colors = { Color.White, Color.Yellow, 
+                           Color.Wheat, Color.WhiteSmoke, 
+                           Color.SlateGray, Color.Blue,
+                                 Color.Pink, Color.Purple, Color.PowderBlue};
+
+        public Planets(
+            int screenWidth,
+            int screenHeight,
+            int planetsCount,
+            Vector2 planetVelocity,
+            Texture2D texture,
+            Rectangle frameRectangle)
+        {
+            this.screenWidth = screenWidth;
+            this.screenHeight = screenHeight;
+            for (int x = 0; x < planetsCount; x++)
+            {
+                planets.Add(new Sprite(
+                    new Vector2(rand.Next(0, screenWidth),
+                        rand.Next(0, screenHeight)),
+                    texture,
+                    frameRectangle,
+                    planetVelocity));
+                Color planetColor = colors[rand.Next(0, colors.Count())];
+                planetColor *= (float)(rand.Next(30, 80) / 100f); planets[planets.Count() - 1].TintColor = planetColor;
+            }
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            foreach (Sprite planet in planets)
+            {
+                planet.Update(gameTime);
+                if (planet.Location.Y > screenHeight)
+                {
+                    planet.Location = new Vector2(
+                        rand.Next(0, screenWidth), 0);
+                }
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            foreach (Sprite planet in planets)
+            {
+                planet.Draw(spriteBatch);
+            }
+        }
+
+    }
+}
---
+++ Asteroid Belt Assault/SpriteCreators.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using System.IO;
+
+namespace Asteroid_Belt_Assault
+{
+    public static class SpriteCreators
+    {
+        public static Dictionary<string, Rectangle> spriteSourceRectangles;
+        public static Random rand = new Random(System.Environment.TickCount);
+
+        public static void Load(string path)
+        {
+            SpriteCreators.spriteSourceRectangles = new Dictionary<string, Rectangle>();
+
+            // open a StreamReader to read the index
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                // while we're not done reading...
+                while (!reader.EndOfStream)
+                {
+                    // get a line
+                    string line = reader.ReadLine();
+
+                    // split at the equals sign
+                    string[] sides = line.Split('=');
+
+                    // trim the right side and split based on spaces
+                    string[] rectParts = sides[1].Trim().Split(' ');
+
+                    // create a rectangle from those parts
+                    Rectangle r = new Rectangle(
+                       int.Parse(rectParts[0]),
+                       int.Parse(rectParts[1]),
+                       int.Parse(rectParts[2]),
+                       int.Parse(rectParts[3]));
+
+                    // add the name and rectangle to the dictionary
+                    SpriteCreators.spriteSourceRectangles.Add(sides[0].Trim(), r);
+                }
+            }
+
+        }
+
+    }
+}
